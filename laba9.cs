ebcf7c451eb340f2e657eb762a9e1251a82355d7
staslif <149203375+staslif@users.noreply.github.com>
2024-01-17 23:57:56 +0300

    laba9.cs

--- laba9.cs
+++ laba9.cs
@@ -21,39 +21,39 @@ class Program
         // Заполняем массив данными
         customers[0] = new Customer()
         {
-            LastName = "Владимир",
-            FirstName = "Владимирович",
-            MiddleName = "Путин",
-            Address = "ул. Пушкина, 137",
-            Phone = "+72286661337",
-            CreditCardNumber = "1234567812345678",
-            BankAccountNumber = "1234567890123456"
+            LastName = "биба",
+            FirstName = "бобович",
+            MiddleName = "бобкинов",
+            Address = "ул. бородина, 23",
+            Phone = "+77654321337",
+            CreditCardNumber = "12132576854655678",
+            BankAccountNumber = "1165768976456756"
         };
 
         customers[1] = new Customer()
         {
-            LastName = "Бебриков",
-            FirstName = "Бебрик",
-            MiddleName = "Бебрикович",
+            LastName = "доктор",
+            FirstName = "кто",
+            MiddleName = "?",
             Address = "ул. Пушкина, 137",
-            Phone = "+72286661337",
-            CreditCardNumber = "8765432187654321",
-            BankAccountNumber = "9876543210987654"
+            Phone = "+79876543210",
+            CreditCardNumber = "1324576898798091",
+            BankAccountNumber = "0789675436576587"
         };
 
         customers[2] = new Customer()
         {
-            LastName = "Пузренков",
-            FirstName = "Александр",
-            MiddleName = "Николаевич",
-            Address = "ул. Пушкина, 137",
-            Phone = "+72286661337",
-            CreditCardNumber = "1111222233334444",
-            BankAccountNumber = "4444555566667777"
+            LastName = "ласковый",
+            FirstName = "нежный",
+            MiddleName = "зверь",
+            Address = "ул. меджиксити, 13",
+            Phone = "+73546576896",
+            CreditCardNumber = "1123211233211232",
+            BankAccountNumber = "1661161616611661"
         };
 
         // Заданный номер кредитной карты
-        string targetCreditCardNumber = "1111222233334444";
+        string targetCreditCardNumber = "1324576898798091";
 
         // Выводим список покупателей, номер кредитной карты которых совпадает с заданным
         Console.WriteLine("Список покупателей с заданным номером кредитной карты:");
